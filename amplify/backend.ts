715a79f0f53963cbe126eea649eb7bba72317624
uttkarsh-8 <uttkarsh.srivastava12@gmail.com>
2025-04-26 17:32:28 +0530

    connect to bedrock

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,29 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { auth } from './auth/resource';
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
+  'bedrockDS',
+  'https://bedrock-runtime.ap-south-1.amazonaws.com',
+  {
+    authorizationConfig: {
+      signingRegion: 'ap-south-1',
+      signingServiceName: 'bedrock',
+    },
+  }
+);
+
+bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
+  new PolicyStatement({
+    resources: [
+      'arn:aws:bedrock:ap-south-1::foundation-model/anthropic.claude-3-5-sonnet-20241022-v2:0',
+    ],
+    actions: ['bedrock:InvokeModel'],
+  })
+);
